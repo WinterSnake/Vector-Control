603b0b5346aed83b9b3b8331cf76369fcfb0bbce
Ryan Smith <smith.a.ryan.98@gmail.com>
2022-04-24 22:18:42 -0400

    update: save/load terminal state

--- src/engine/window.cpp
+++ src/engine/window.cpp
@@ -2,11 +2,19 @@
     TextEngine
     - UNIX Terminal Window
 */
+#include <stdio.h>
+#include "engine/window.hpp"
+
 #define STDIN  0
 #define STDOUT 1
 #define STDERR 2
-#include <stdio.h>
-#include "engine/window.hpp"
+
+#define SCREEN_SAVE  "\x1B[?47h"
+#define SCREEN_LOAD  "\x1B[?47l"
+#define SCREEN_CLEAR "\x1B[2J"
+#define MOUSE_SAVE   "\x1B 7\x1B[s"
+#define MOUSE_LOAD   "\x1B 8\x1B[u"
+#define MOUSE_RESET  "\x1B[H"
 
 namespace Engine
 {
@@ -17,11 +25,13 @@ namespace Engine
         tcgetattr(STDOUT, &Window::term);
         Window::term.c_lflag &= ~ECHO & ~ICANON;
         tcsetattr(STDOUT, TCSANOW, &term);
+        printf(MOUSE_SAVE SCREEN_SAVE SCREEN_CLEAR MOUSE_RESET);
     }
 
     void Window::End_Screen()
     {
         Window::term.c_lflag |= ECHO | ICANON;
         tcsetattr(STDOUT, TCSANOW, &term);
+        printf(SCREEN_LOAD MOUSE_LOAD);
     }
 }
